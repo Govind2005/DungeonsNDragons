e5d0688f3b794d0627d5274a60e11f6dc8391953
Achir Jain <achirjain11@gmail.com>
2026-03-31 18:54:11 +0530

    add backend folder structure

---
+++ backend/lobbyManager/src/main/java/com/dungeonsNdragons/lobbyManager/LobbyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dungeonsNdragons.lobbyManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobbyManagerApplication.class, args);
+	}
+
+}
