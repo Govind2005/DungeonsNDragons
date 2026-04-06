eea8d060638869d4ecaa53bef07aabfda5485c1f
Govind2005 <realgovindkrishna@gmail.com>
2026-04-07 00:17:02 +0530

    bugs fixed at frontend

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
