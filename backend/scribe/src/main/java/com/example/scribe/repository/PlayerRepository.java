99f998d9e579112911a6f2d4f983e5ca5eba90a8
Achir Jain <achirjain11@gmail.com>
2026-04-09 22:19:56 +0530

    remove unused services

--- backend/scribe/src/main/java/com/example/scribe/repository/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.scribe.repository;
-
-import com.example.scribe.entity.PlayerProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerRepository extends JpaRepository<PlayerProfile, String> {
-}
