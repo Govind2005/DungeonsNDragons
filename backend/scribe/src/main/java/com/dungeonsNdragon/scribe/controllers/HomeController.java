a45171c2749066071fa5c1b9a17c772fa7a480e8
OmSikawat <omsikawat67@gmail.com>
2026-04-16 16:19:57 +0530

    Add Home Controller

---
+++ backend/scribe/src/main/java/com/dungeonsNdragon/scribe/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.dungeonsNdragon.scribe.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+    @RequestMapping("")
+    public ResponseEntity<?> home() {
+        return new ResponseEntity<>("Welcome to the Dungeons & Dragons Bouncer API!", HttpStatus.OK);
+    }
+}
